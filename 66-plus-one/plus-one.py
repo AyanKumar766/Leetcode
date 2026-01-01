da01d3e1e02e86a7e61bc494ccae5eff1249caac
Ayan Kumar <110765869+AyanKumar766@users.noreply.github.com>
2026-01-01 15:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (82.42%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        pointer= len(digits) - 1        
+        while digits[pointer] == 9:
+            digits[pointer] = 0
+            pointer -= 1        
+        if pointer == -1:
+            digits.insert(0, 1)        
+        else:
+            digits[pointer] += 1        
+        return digits
